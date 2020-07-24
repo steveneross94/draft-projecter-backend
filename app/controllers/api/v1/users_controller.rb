93f7e6528134a2147b369829795790b5f8eeec70
steveneross94 <ross.steven@me.com>
2020-07-24 16:34:34 -0400

    backend update

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,32 @@
 class Api::V1::UsersController < ApplicationController
     def index
-        @users = User.all
+       user = User.all
+        render json:user
+    end 
+
+    def create
+        user = User.create!(user_params)
+        render json: user
+    end
+
+    def destroy
+        user = User.find(params[:id])
+        user.destroy!
+        render json: {}
+    end
+
+    def show
+        user = User.find(params[:id])
+        render json: user
+    end
+
+    def update
+        user = User.find(params[:id])
+        user.update(user_params)
+        render json: user
+    end
+
+    def user_params
+        params.require(:user).permit!
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,22 +2,22 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources 
+      resources :players
     end
   end
   namespace :api do
     namespace :v1 do
-      resources 
+      resources :users
     end
   end
   namespace :api do
     namespace :v1 do
-      resources 
+      resources :teams
     end
   end
   namespace :api do
     namespace :v1 do
-      resources 
+      resources :rosters
     end
   end
 end

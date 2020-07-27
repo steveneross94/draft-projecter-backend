4773607aac3dc9129e967a390f40ef1595eea809
grantkoji <grantkoji@gmail.com>
2020-07-27 17:57:32 -0400

    with roster controller

--- app/controllers/api/v1/rosters_controller.rb
+++ app/controllers/api/v1/rosters_controller.rb
@@ -4,4 +4,30 @@ class Api::V1::RostersController < ApplicationController
         render json: rosters
     end
 
+    def create
+        roster = Roster.create!(roster_params)
+        render json: roster
+    end
+
+    def destroy
+        roster = Roster.find(params[:id])
+        roster.destroy!
+        render json: {}
+    end
+
+    def show
+        roster = Roster.find(params[:id])
+        render json: roster
+    end
+
+    def update
+        roster = Roster.find(params[:id])
+        roster.update(roster_params)
+        render json: roster
+    end
+
+    def roster_params
+        params.require(:roster).permit!
+    end
+
 end

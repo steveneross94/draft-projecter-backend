fc484c396b74f2b7c36baa3a48115f3f9adf0603
steveneross94 <ross.steven@me.com>
2020-07-29 16:59:20 -0400

    final project done

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -8,12 +8,12 @@ class Api::V1::ArticlesController < ApplicationController
         article = fetch_url("http://newsapi.org/v2/everything?q=#{params[:id]}&from=2020-07-28&sortBy=popularity&apiKey=#{article_api_key}")
         render json: article
     end 
-      
+    
     def fetch_url(url)
         uri = URI.parse(url)
         response = Net::HTTP.get_response(uri)
         resp_body = response.body
         json_resp = JSON.parse(resp_body)
     end
-      
+    
 end

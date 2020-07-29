25397565cde239a60d5a9ee308f78ea5bbce73a9
grantkoji <grantkoji@gmail.com>
2020-07-28 22:57:47 -0400

    adding articles to database

---
+++ app/controllers/api/v1/articles_controller.rb
@@ -0,0 +1,15 @@
+class Api::V1::ArticlesController < ApplicationController
+    def show
+        article_api_key = ENV['article_api_key']
+        article = fetch_url("http://newsapi.org/v2/everything?q=#{params[:id]}&from=2020-07-28&sortBy=popularity&apiKey=#{article_api_key}")
+        render json: article
+    end 
+      
+    def fetch_url(url)
+        uri = URI.parse(url)
+        response = NET::HTTP.get_response(url)
+        resp_body = response.body
+        json_resp = JSON.parse(resp_body)
+    end
+      
+end
